98ed060adf00d406b6ee201e34260d54dd85a90d
sjpv <sjpv.milan@hotmail.com>
2025-07-23 13:09:19 -0500

    feat: Spread Operator - components

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        setCategories( ["Bleach", ...categories] );
+    };
+
   return (
     <>
+    
+        {/* title */}
         <h1>GitExpertApp</h1>
+
+        {/* input */}
+        <AddCategory />        
+
+        {/* List Items (Gifs) */} 
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+            {
+                categories.map( (category) => {
+                    {/* Gif Item */}
+                    return <li key={category}>{category}</li>
+                } )
+            }
+        </ol>
+            
     </>
   )
 }
